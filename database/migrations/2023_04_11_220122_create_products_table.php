97e5d219affa4e06ef9c25bf179468357ca9e093
medbrs <medimedbouras@gmail.com>
2023-04-14 22:15:46 +0000

    update migration files

--- database/migrations/2023_04_11_220122_create_products_table.php
+++ database/migrations/2023_04_11_220122_create_products_table.php
@@ -16,14 +16,22 @@ return new class extends Migration
             $table->string('name');
             $table->text('description');
             $table->decimal('price', 10, 2);
-            $table->string('image');
             $table->integer('quantity');
             $table->timestamps();
+        });
+
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->string('url');
+            $table->timestamps();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
     public function down()
     {
+        Schema::dropIfExists('product_images');
         Schema::dropIfExists('products');
     }
 };
---
+++ database/migrations/2023_04_14_220314_create_product_categories_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {   
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('parent')->nullable();
+            $table->foreign('parent')->references('id')->on('categories')->onDelete('cascade');
+            $table->unsignedBigInteger('id_parent')->nullable();
+            $table->foreign('id_parent')->references('id')->on('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {   
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('product_categories');
+    }
+};
